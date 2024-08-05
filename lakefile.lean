597f69e4e117539dd1f6dd3fafdb592db1c1dfd8
Sean Welleck <wellecks@gmail.com>
2024-08-05 19:18:50 -0400

    bump to v4.11.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «llmlean» {
   -- add any package configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "600a5fa3828fef53b2fa20d30dc8e1fb51ce0f98"
+require mathlib from git "https://github.com/leanprover-community/mathlib4"
 
 @[default_target]
 lean_lib «LLMlean» {
