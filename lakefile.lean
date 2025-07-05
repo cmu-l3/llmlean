fd1c899ef47b2711c6062bc3a5864341cf81ac31
Thomas Zhu <thomas.zh@bytedance.com>
2025-07-04 18:38:04 -0700

    change LLMleanTest into its own project

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,3 @@ package «llmlean» where
 @[default_target]
 lean_lib «LLMlean» where
   -- add any library configuration options here
-
--- If -Ktest=on is passed, then add additional requirements
-meta if get_config? test = some "on" then do
-  require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.20.0-rc5"
-
-  lean_lib LLMleanTest where
-    globs := #[.submodules `LLMleanTest]
